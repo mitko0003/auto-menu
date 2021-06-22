846bda77c6cc977649456e765df03ae08ccd904d
Димитър Трендафилов <dimitar.trendafilov94@gmail.com>
2021-06-22 20:27:31 +0300

    Forgotten changes for previous commit

--- src/menu.py
+++ src/menu.py
@@ -11,6 +11,11 @@ NAMES_OFFSET = 1
 
 LANGUAGES = []
 
+UNITS = {
+    "gram" = "гр/gr",
+    "milliliter" = "мл/ml"
+}
+
 def normalize(value):
     return '' if value is None else re.sub("[\ufeff]", '', value.strip())
 
@@ -19,6 +24,23 @@ class Language:
         self.txt_index = txt_index + NAMES_OFFSET
         self.excel_index = excel_index
         LANGUAGES.append(self)
+
+class Row:
+    def __init__(self, name, weight, price):
+        self.name = name
+        self.weight = weight
+        self.price = price
+
+class Section:
+    def __init__(self, title):
+        self.title = title
+        self.rows = []
+        self.order = 1
+
+class Page:
+    def __init__(self):
+        self.sections = []
+        self.order = 1
 
 BG = Language(0, 0)
 EN = Language(1, 1)
